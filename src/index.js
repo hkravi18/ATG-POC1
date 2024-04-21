2e9e7a6b9e65af71758cceca5292505dae1f2c8f
Harshit Kumar Ravi <hkravi2002@gmail.com>
2024-04-21 14:09:38 +0530

    refactor: establish database connection

--- src/index.js
+++ src/index.js
@@ -17,20 +17,31 @@ const companyRoutes = require("./routes/companyRoutes.js");
 //logger
 const customLogger = require("./middlewares/logger.js");
 
+//db
+const establishConnection = require("./db/establishConnection.js");
+
 app.use(customLogger);
 
 app.use("/api/company", companyRoutes);
 
 app.use(errorHandler);
 
-app.on("error", (err) => {
-  if (err instanceof Error) {
-    console.log("ERROR (App error): " + err?.message);
-  } else {
-    console.log("ERROR (App error): An unknown error occurred");
-  }
-});
+establishConnection
+  .then(() => {
+    console.log("Database setup complete.");
 
-app.listen(port, () => {
-  console.log(`Server listening on ${port}\n`);
-});
+    app.on("error", (err) => {
+      if (err instanceof Error) {
+        console.log("ERROR (App error): " + err?.message);
+      } else {
+        console.log("ERROR (App error): An unknown error occurred");
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`Server listening on ${port}\n`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to setup database:", err.message);
+  });
